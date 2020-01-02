fe83c4a187b4da04ab67f64884db7cd43df23a8b
Raite17 <survivarian@gmail.com>
2020-01-03 00:26:15 +0600

    UI part of creating and editing posts

--- store/post.js
+++ store/post.js
@@ -1,15 +1,41 @@
+import { create } from "domain"
+
+const posts = [
+    { title: 'Post1', date: new Date(), views: 22, comments: [1, 2], _id: Math.random() },
+    { title: 'Post2', date: new Date(), views: 22, comments: [1, 2], _id: Math.random() }
+]
+
 export const actions = {
     async fetchAdmin({}) {
         return await new Promise(resolve => {
             setTimeout(() => {
-                resolve([
-                    { title: 'Post1', date: new Date(), views: 22, comments: [1, 2], _id: Math.random() },
-                    { title: 'Post2', date: new Date(), views: 22, comments: [1, 2], _id: Math.random() }
-                ])
+                resolve(posts)
             }, 1000)
         })
     },
+
+    async create({}, { title, text }) {
+
+        return await new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+            }, 1000)
+        })
+    },
+
     async remove({}, id) {
 
+    },
+
+    async update({}, { id, text }) {
+
+    },
+
+    async fetchAdminById({}, id) {
+        return await new Promise(resolve => {
+            setTimeout(() => {
+                resolve(posts.find(p => p._id = id));
+            }, 1000)
+        })
     }
 }
